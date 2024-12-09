3ab06da41523b4a11df176a4408cfe81ee7534c6
Kaio <128155244+Kaio0809@users.noreply.github.com>
2024-12-09 11:35:50 +0000

    calc

--- calculadora_oo.py
+++ calculadora_oo.py
@@ -1,18 +1,34 @@
 class Calculadora:
     def __init__(self):
-        pass
+        self.__acumulador = 0.0
 
-    def soma(self,operando_a,operando_b):
-        return operando_a + operando_b
+    def soma(self,operando_b,operando_a = None):
+        if operando_a is None:
+            self.__acumulador += operando_b
+        else:
+            self.__acumulador = operando_a + operando_b
+        return self.__acumulador
     
-    def sub(self,operando_a,operando_b):
-        return operando_a - operando_b
+    def sub(self,operando_b, operando_a = None):
+        if operando_a is None:
+            self.__acumulador -= operando_b
+        else:
+            self.__acumulador = operando_a - operando_b
+        return self.__acumulador
     
-    def mult(self,operando_a,operando_b):
-        return operando_a*operando_b
+    def mult(self,operando_b,operando_a = None):
+        if operando_a is None:
+            self.__acumulador *= operando_b
+        else:
+            self.__acumulador = operando_a * operando_b
+        return self.__acumulador
     
-    def div(self,operando_a,operando_b):
+    def div(self,operando_b,operando_a = None):
         try:
-            return operando_a/operando_b
+            if operando_a is None:
+                self.__acumulador /= operando_b
+            else:
+                self.__acumulador = operando_a / operando_b
+            return self.__acumulador
         except ZeroDivisionError:
             return "Erro de Divisão por Zero!"
--- testa_calculadora_oo.py
+++ testa_calculadora_oo.py
@@ -6,6 +6,10 @@ def teste():
     print(calc.sub(15,10))
     print(calc.mult(10,15))
     print(calc.div(10,15))
-    print(calc.div(10,0))
+    print(calc.div(0))
+    print(calc.soma(10))
+    print(calc.sub(9))
+    print(calc.mult(10))
+    print(calc.div(2))
 
 teste()
